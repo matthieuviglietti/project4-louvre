8bada2fb0433f78a67c4b1ca2a1d509991343f5f
Viglietti Matthieu <vigliettimatthieu@gmail.com>
2019-01-15 15:59:35 +0100

    configuration locale, starting building routes and index

--- Louvre/src/MV/BookingBundle/Controller/DefaultController.php
+++ Louvre/src/MV/BookingBundle/Controller/DefaultController.php
@@ -1,13 +1,20 @@
 <?php
 
 namespace MV\BookingBundle\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($name)
     {
-        return $this->render('MVBookingBundle:Default:index.html.twig');
+        return $this->render('@MVBooking/Default/index.html.twig',array(
+        'name' => $name));
+    }
+
+    public function translationAction(Request $request)
+    {
+        return $this->render('@MVBooking/Default/index.html.twig');
     }
 }
