bf1c5fde87e153fbca1e646b27e38774ff70b046
Viglietti Matthieu <vigliettimatthieu@gmail.com>
2019-01-17 13:54:02 +0100

    generate general layout, bundle layout with footer, stylesheets and h1

---
+++ Louvre/src/MV/BookingBundle/Controller/BookingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MV\BookingBundle\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BookingController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@MVBooking/Default/index.html.twig');
+    }
+
+    public function translationAction(Request $request)
+    {
+        return $this->render('@MVBooking/Default/date.html.twig');
+    }
+}
