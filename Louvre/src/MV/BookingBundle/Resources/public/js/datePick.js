5638c1ea81c6af91ac5732eeee6893c451defc87
Viglietti Matthieu <vigliettimatthieu@gmail.com>
2019-01-18 16:22:46 +0100

    configuration of datepicker jquery ui in fr install jquery and jquery ui

---
+++ Louvre/src/MV/BookingBundle/Resources/public/js/datePick.js
@@ -0,0 +1,40 @@
+$(function(){
+    $.datepicker.setDefaults( $.datepicker.regional[ "fr" ] );
+    $('#date').datepicker({
+        dateFormat : 'dd-mm-yy',
+        minDate : 0,
+        appendText: "format attendu : jj-mm-aaaa",
+        firstDay: 1,
+        beforeShowDay: DisableDates,
+        /*beforeShowDay: DisableSpecificDates*/
+    });
+
+function DisableDates(date) {
+
+     /** Days to be disabled as an array */
+    let disableddates = ["5-1-2019", "12-25-2019", "11-1-2019"];
+ 
+    var day = date.getDay();
+    var m = date.getMonth();
+    var d = date.getDate();
+    var y = date.getFullYear();
+
+ for (var i = 0; i < disableddates.length; i++) {
+        // First convert the date in to the mm-dd-yyyy format 
+        // Take note that we will increment the month count by 1 
+        var currentdate = (m + 1) + '-' + d + '-' + y ;
+        console.log(currentdate);
+   // If day == 1 then it is MOnday
+   if (day == 2) {
+   
+    return [false] ; {
+    }
+}
+    else if ($.inArray(currentdate, disableddates) != -1 ){
+        return [false];
+   
+   } else { 
+   return [true] ;
+   }
+}}
+});
