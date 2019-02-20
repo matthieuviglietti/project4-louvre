1317081cd3b3ebf94abcd7a3792ae19d9aa8359d
matthieuviglietti <vigliettimatthieu@gmail.com>
2019-02-20 16:21:21 +0100

    Create Order as a service

---
+++ Louvre/src/MV/BookingBundle/Order/MVCreateOrder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MV\BookingBundle\Order;
+use MV\BookingBundle\Entity\Command;
+
+class MVCreateOrder
+{
+    public function createOrder($em, $sessionId, $amount, $email)
+    {
+        $createOrder = new Command();
+        $createOrder->setSpecialKey($sessionId)
+            ->setCharge($amount)
+            ->setEmail($email);
+
+        $em->persist($createOrder);
+        $em->flush();
+    }
+
+}
